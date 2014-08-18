f8f0b222d2903c03cb974cec7978ddf46a012c81
Thiago Cardoso <thiagoncc@gmail.com>
2014-08-18 09:52:26 -0300

    Changing messages to avoid reply
    In order to a tweet appear in the regular timeline it can't start with a
    mention.

--- messages.go
+++ messages.go
@@ -5,15 +5,15 @@ package main
 // author's screen name.
 var messages = []string{
 	"Hey @%s you are truly an artist.",
-	"@%s good artists copy, great artists steal!",
-	"@%s I can see some patterns!",
-	"@%s there are some nice colors there!",
-	"@%s our new Picasso!",
-	"@%s like Michelangelo you should order it to speak",
+	"Hi @%s , good artists copy, great artists steal!",
+	"Hi @%s , I can see some patterns!",
+	"Hi @%s , there are some nice colors there!",
+	"Is @%s our new Picasso?",
+	"Hey @%s , like Michelangelo you should order it to speak",
 	"Look at the new paint made by @%s",
 	"The next item is this beautiful painting by @%s - any bids?",
 	"I am going to buy this one @%s",
 	"Should I buy this one @%s ?",
-	"@%s awesome!",
-	"@%s this is my favorite contemporanean art",
+	"Hey @%s , that's awesome!",
+	"Hey @%s , this is my favorite contemporanean art",
 }
